a7238aa215610062084d6847e3e1cdf1160e0941
Andres Vazquez <andres.vazquez.martin@gmail.com>
2023-10-27 23:05:30 +0200

    added ApartmentsPage connected to the API

---
+++ src/pages/ApartmentsPage.jsx
@@ -0,0 +1,32 @@
+// src/pages/ApartmentsPage.js
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function ApartmentsPage() {
+  const [apartments, setApartments] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://ironbnb-m3.herokuapp.com/apartments')
+      .then((response) => setApartments(response.data));
+  }, []);
+
+  return (
+    <div>
+      <h3>List of apartments</h3>
+      <br />
+      {apartments.map((apartment) => {
+        return (
+          <div key={apartment._id}>
+            <h3>{apartment.title}</h3>
+            <img src={apartment.img} alt="apartment" />
+            <p>Price: {apartment.pricePerDay}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ApartmentsPage;
